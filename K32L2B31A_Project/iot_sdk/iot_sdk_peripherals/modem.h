863227ccd97fc0d1a8f49bbba2174244ac488c82
Camilo Mozo <camilomozov@gmail.com>
2021-11-08 20:39:58 -0500

    Primera Integracion
    Codigo base del proyecto de IoT, incluye un canal adc analogico abierto para el sensor  e integra el modulo para hacer uso del modem.

---
+++ K32L2B31A_Project/iot_sdk/iot_sdk_peripherals/modem.h
@@ -0,0 +1,52 @@
+/*! @file : modem.h
+ * @author  JAVIER ELIAS TOBON AYUBB
+ * @version 1.0.0
+ * @date    12/10/2021
+ * @brief   Driver para 
+ * @details
+ *
+ */
+#ifndef IOT_SDK_PERIPHERALS_MODEM_H_
+#define IOT_SDK_PERIPHERALS_MODEM_H_
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+
+
+/*!
+ * @addtogroup X
+ * @{
+ */
+/*!
+ * @addtogroup X
+ * @{
+ */
+/*******************************************************************************
+ * Public Definitions
+ ******************************************************************************/
+#define TIME_WAIT_RTA_CMD 2 //[=] Segundos que espera una Respuesta del Modem
+//despues de enviar un comando
+
+#define Modem_Send_Cmd(comandoStr) printf(comandoStr)
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Public vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Public Prototypes
+ ******************************************************************************/
+// Incicializa variables y estado inicial del Modem
+// configura y inicia proceso de enviar un primer mensaje
+void Modem_Init(void);
+
+// Funcion de estado estable
+void Modem_Task_Run(void);
+
+/** @} */ // end of X group
+/** @} */ // end of X group
+
+#endif /* IOT_SDK_PERIPHERALS_MODEM_H_ */
---
+++ K32L2B31A_Project/source/K32L2B31A_Project.c
@@ -0,0 +1,86 @@
+/*
+* @author  JAVIER ELIAS TOBON AYUBB
+ * @version 1.0.0
+ * @date    ${date}
+ * @brief   Driver para
+ * @details
+ *
+*/
+/**
+ * @file    K32L2B31A_Project.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "K32L2B31A.h"
+#include "fsl_debug_console.h"
+/* TODO: insert other include files here. */
+
+#include "irq_lptmr0.h"
+#include "irq_lpuart0.h"
+#include "modem.h"
+
+/* TODO: insert other definitions and declarations here. */
+
+#define SIZE_BUFFER_COMANDO	105
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+volatile uint32_t segAct=0;
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+
+// Inicializa Alarma
+void Alarma_Init(void){
+	segAct = lptmr0_irqCounter;
+}
+
+// retorna el tiempo futuro de la alarma
+uint32_t Alarma_Set(uint32_t time2Wait){
+	segAct = lptmr0_irqCounter;
+	return (segAct + time2Wait);
+}
+
+// retorna si la alarma se cumplio
+char Alarma_Elapsed(uint32_t time2Test){
+	segAct = lptmr0_irqCounter;
+	if(segAct >= time2Test) return true;
+	else return false;
+}
+
+#define Timer_Init() LPTMR_StartTimer(LPTMR0)
+
+/* Init board hardware. */
+void BOARD_Init(void){
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+}
+
+//  main
+int main(void) {
+	BOARD_Init();
+    Timer_Init();
+    Modem_Init();
+    Alarma_Init();
+    while(1) {
+    	Modem_Task_Run();
+    }
+    return 0 ;
+}
