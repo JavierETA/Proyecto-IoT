30dced16b40bb2d6f8a2af9f3cf17161d43dd416
Javier Tobon <javier.elias.tobon.ayubb30@gmail.com>
2021-12-14 19:04:32 -0500

    Conexion wifi
    Se realiza pequeño cambio que evitaba la conexion wifi exitosa

--- ESP32/main.py
+++ ESP32/main.py
@@ -6,8 +6,8 @@ from umqttsimple import MQTTClient
 # CONSTANTES
 
 # CREDENCIALES WIFI
-SSID = "7629616296b6"
-PASSWORD = "e0c60effb0f8"
+SSID = "Geronimomr"
+PASSWORD = "Luana13082009611**"
 wificont = 0
 # FIN CREDENCIALES WIFI
 
@@ -37,22 +37,18 @@ adcMeta.atten(ADC.ATTN_11DB)
 # funcion de conexion wifi
 def conectar(SSID, PASSWORD):
     global sta_if, wificont
-    sta_if = network.WLAN(
-        network.STA_IF
-    )  # instancia el objeto -sta_if- para controlar la interfaz STA
-    if not sta_if.isconnected():  # si no existe conexión...
-        sta_if.active(True)  # activa el interfaz STA del ESP32
-        scann = sta_if.scan()  # Se realiza una busqueda de las redes wifi disponibles
+    sta_if = network.WLAN(network.STA_IF)     # instancia el objeto -sta_if- para controlar la interfaz STA
+    if not sta_if.isconnected():              # si no existe conexión...
+        sta_if.active(True)               # activa el interfaz STA del ESP32
+        scann = sta_if.scan()
         for i in range(len(scann)):
             busc = scann[i][0]
             busc = busc.decode("UTF-8")
-            if (
-                busc.find(SSID) >= 0
-            ):  # Condicion que busca si la red wifi esta accesible
-                sta_if.connect(SSID, PASSWORD)  # inicia la conexión con el AP
-                print("Conectando a la red", SSID + "...")
+            if (busc.find(SSID) >= 0):  # Condicion que busca si la red wifi esta accesible
                 wificont = 1
         if wificont == 1:
+            sta_if.connect(SSID, PASSWORD)            # inicia la conexión con el AP
+            print('Conectando a la red', SSID +"...")
             if sta_if.isconnected():
                 return
             else:
@@ -60,8 +56,8 @@ def conectar(SSID, PASSWORD):
                 pass
         else:
             return
-        while not sta_if.isconnected():  # ...si no se ha establecido la conexión...
-            pass  # ...repite el bucle...
+        while not sta_if.isconnected():           # ...si no se ha establecido la conexión...
+            pass                                  # ...repite el bucle...
 
 
 # ----------------------------------
